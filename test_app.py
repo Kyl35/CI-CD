8125b11867bf675f6f66ff4fa1fbbbdda82dab84
Kyl35 <165373407+Kyl35@users.noreply.github.com>
2024-04-04 09:44:05 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,9 +11,9 @@ class FlaskTestCase(unittest.TestCase):
     response = self.app.get('/')
     self.assertEqual(response.status_code, 200)
 
-def test_info_status_code(self):
+  def test_info_status_code(self):
     response = self.app.get('/info')
     self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
-unittest.main()
+  unittest.main()
